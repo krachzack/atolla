63ff9178dac7b34e20ffcd7946f855ac69c1c547
Philipp Stadler <hello@phstadler.com>
2017-03-03 18:11:43 +0100

    Implemented basic source functionality

--- protocol/include/atolla/source.h
+++ protocol/include/atolla/source.h
@@ -10,7 +10,7 @@ enum AtollaSourceState
     // Channel is waiting for a response from the other end or the device was
     // relent
     ATOLLA_SOURCE_STATE_WAITING,
-    // Channel is open and ready for reading and writing
+    // Communcation is ongoing
     ATOLLA_SOURCE_STATE_OPEN
 };
 typedef enum AtollaSourceState AtollaSourceState;
@@ -33,10 +33,21 @@ void atolla_source_free(AtollaSource source);
 AtollaSourceState atolla_source_state(AtollaSource source);
 
 /**
+ * Determines how many frames have to be sent to the sink to fill its buffer
+ * at the instant of calling the function.
+ *
+ * If this function returns a non-zero value, the atolla_source_put function
+ * will never block. In the zero case, the put function will halt until the
+ * device is ready to receive the next frame.
+ */
+int atolla_source_frame_lag(AtollaSource source);
+
+/**
  * Tries to enqueue the given frame in the connected sink.
  *
  * The call might block for some time in order to wait for the device to catch
- * up displaying the previously sent frames.
+ * up displaying the previously sent frames. If atolla_source_frame_lag returns
+ * a non-zero value, atolla_source_put is guaranteed not to wait.
  */
 bool atolla_source_put(AtollaSource source, void* frame, size_t frame_len);
 
--- protocol/src/atolla/source.c
+++ protocol/src/atolla/source.c
@@ -1,35 +1,40 @@
 #include "atolla/source.h"
+#include "msg/builder.h"
+#include "msg/iter.h"
+#include "test/assert.h"
+#include "time/now.h"
+#include "time/sleep.h"
 #include "udp_socket/udp_socket.h"
 
 #include <stdlib.h>
 
-#include "test/assert.h"
-#include "atolla/sleep_ms.h"
-
 static const size_t recv_buf_len = 3 + 65537;
-static const size_t send_buf_len = 3 + 65537;
 
 struct AtollaSourcePrivate
 {
     AtollaSourceState state;
-    void* recv_buf;
-    void* send_buf;
     UdpSocket sock;
+    void* recv_buf;
+    
+    MsgBuilder builder;
+
+    int next_frame_idx;
     int frame_length_ms;
     int max_buffered_frames;
-    int lag_ms; // How many ms are we behind schedule in sending frames
+    int last_frame_time;
 };
 typedef struct AtollaSourcePrivate AtollaSourcePrivate;
 
+static AtollaSourcePrivate* source_private_make(int frame_length_ms, int max_buffered_frames);
+static void source_send_borrow(AtollaSourcePrivate* source);
+static void source_update(AtollaSourcePrivate* source);
+static void iterate_recv_buf(AtollaSourcePrivate* sink, size_t received_bytes);
+
 AtollaSource atolla_source_make(const char* sink_hostname, int sink_port, int frame_length_ms, int max_buffered_frames)
 {
-    AtollaSourcePrivate* source = (AtollaSourcePrivate*) malloc(sizeof(AtollaSourcePrivate));
-    source->state = ATOLLA_SOURCE_STATE_WAITING;
-    source->recv_buf = malloc(recv_buf_len);
-    source->send_buf = malloc(send_buf_len);
-    source->frame_length_ms = frame_length_ms;
-    source->max_buffered_frames = max_buffered_frames;
-    source->lag_ms = max_buffered_frames * frame_length_ms;
+    AtollaSourcePrivate* source = source_private_make(frame_length_ms, max_buffered_frames);
+
+    msg_builder_init(&source->builder);
 
     UdpSocketResult result;
     
@@ -39,8 +44,24 @@ AtollaSource atolla_source_make(const char* sink_hostname, int sink_port, int fr
     result = udp_socket_set_receiver(&source->sock, sink_hostname, (unsigned short) sink_port);
     assert(result.code == UDP_SOCKET_OK);
 
+    source_send_borrow(source);
+
     AtollaSource source_handle = { source };
     return source_handle;
+}
+
+static AtollaSourcePrivate* source_private_make(int frame_length_ms, int max_buffered_frames)
+{
+    AtollaSourcePrivate* source = (AtollaSourcePrivate*) malloc(sizeof(AtollaSourcePrivate));
+
+    source->state = ATOLLA_SOURCE_STATE_WAITING;
+    source->recv_buf = malloc(recv_buf_len);
+    source->next_frame_idx = 0;
+    source->frame_length_ms = frame_length_ms;
+    source->max_buffered_frames = max_buffered_frames;
+    source->last_frame_time = 0;
+
+    return source;
 }
 
 void atolla_source_free(AtollaSource source_handle)
@@ -57,7 +78,15 @@ AtollaSourceState atolla_source_state(AtollaSource source_handle)
 {
     AtollaSourcePrivate* source = (AtollaSourcePrivate*) source_handle.private;
 
+    source_update(source);
+
     return source->state;
+}
+
+int atolla_source_frame_lag(AtollaSource source_handle)
+{
+    AtollaSourcePrivate* source = (AtollaSourcePrivate*) source_handle.private;
+    return (time_now() - source->last_frame_time) / source->frame_length_ms;
 }
 
 /**
@@ -68,16 +97,81 @@ AtollaSourceState atolla_source_state(AtollaSource source_handle)
  */
 bool atolla_source_put(AtollaSource source_handle, void* frame, size_t frame_len)
 {
-    AtollaSourcePrivate* source = (AtollaSourcePrivate*) source_handle.private;
-
-    if(source->lag_ms < 0)
+    if(atolla_source_state(source_handle) != ATOLLA_SOURCE_STATE_OPEN)
     {
-        sleep_ms(source->lag_ms);
+        return false;
     }
 
-    // TODO send the enqueued frame
+    AtollaSourcePrivate* source = (AtollaSourcePrivate*) source_handle.private;
 
-    source->lag_ms -= source->frame_length_ms;
+    if(atolla_source_frame_lag(source_handle) == 0)
+    {
+        // If the receiving device has no space in the buffer to hold new frames,
+        // wait for the duration of one frame
+        time_sleep(source->frame_length_ms);
+    }
+
+    MemBlock* enqueue_msg = msg_builder_enqueue(&source->builder, source->next_frame_idx, frame, frame_len);
+
+    udp_socket_send(&source->sock, enqueue_msg->data, enqueue_msg->size);
+
+    ++source->next_frame_idx;
+    source->last_frame_time += source->frame_length_ms;
 
     return true;
+}
+
+static void source_send_borrow(AtollaSourcePrivate* source)
+{
+    MemBlock* borrow_msg = msg_builder_borrow(&source->builder, source->frame_length_ms, source->max_buffered_frames);
+    udp_socket_send(&source->sock, borrow_msg->data, borrow_msg->size);
+}
+
+static void source_update(AtollaSourcePrivate* source)
+{   
+    size_t received_len;
+    UdpSocketResult result;
+
+    result = udp_socket_receive(
+        &source->sock,
+        source->recv_buf, recv_buf_len,
+        &received_len
+    );
+
+    if(result.code == UDP_SOCKET_OK)
+    {
+        iterate_recv_buf(source, received_len);
+    }
+}
+
+static void iterate_recv_buf(AtollaSourcePrivate* source, size_t received_bytes)
+{
+    MsgIter iter = msg_iter_make(source->recv_buf, received_bytes);
+
+    while(msg_iter_has_msg(&iter))
+    {
+        MsgType type = msg_iter_type(&iter);
+
+        switch(type)
+        {
+            case MSG_TYPE_LENT:
+            {
+                source->state = ATOLLA_SOURCE_STATE_OPEN;
+                source->last_frame_time = time_now() - source->max_buffered_frames * source->frame_length_ms;
+                break;
+            }
+
+            case MSG_TYPE_FAIL:
+            {
+                source->state = ATOLLA_SOURCE_STATE_ERROR;
+                break;
+            }
+
+            default:
+            {
+                assert(false);
+                break;
+            }
+        }
+    }
 }
