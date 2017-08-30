9ac299e30418f3d6f899a2706ac88a3958b90868
krachzack <hello@phstadler.com>
2017-08-30 15:10:16 +0200

    time_now is now unsigned and works on arduino

--- protocol/src/time/now.c
+++ protocol/src/time/now.c
@@ -1,11 +1,20 @@
 #include "time/now.h"
-#include "time/gettime.h"
 
-int time_now()
+#ifdef HAVE_ARDUINO_SLEEP
+#include <Arduino.h>
+#else
+#include "time/gettime.h"
+#endif
+
+unsigned int time_now()
 {
+#ifdef HAVE_ARDUINO_SLEEP
+    return millis();
+#else
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
 
     return ts.tv_sec * 1000 +
            ts.tv_nsec / 1000000;
+#endif
 }
